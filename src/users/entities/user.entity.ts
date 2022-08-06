826a544b640fad348859dedb147c43a2ed611f1c
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-06 22:26:59 +0530

    Entity Defined

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    email: string;
+}
