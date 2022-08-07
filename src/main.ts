a8d097a58663eba85b095c5bbe291ff118932a80
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-07 18:26:55 +0530

    CORS Enabled

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Enable CORS
+  app.enableCors();
+
   // Listening Port
   await app.listen(process.env.PORT || 3000);
 }
